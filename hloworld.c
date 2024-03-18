099e054e1597700b22aa599168b7ff254ab2519e
Vinay Math <131176204+01fe22bei012@users.noreply.github.com>
2024-03-18 13:16:01 +0530

    Add files via upload

--- hloworld.c
+++ hloworld.c
@@ -1,10 +1,23 @@
 #include<stdio.h>
-#include<ctype.h>
+#include<stdlib.h>
+#include<string.h>
 
-int main()
-{
-    char a[10];
-    gets(a);
-    puts(a);
+int main(){
+FILE*file;
+char*message ="HELLO WORLD";
+char read_message[100];
 
+//write a message to file
+file =fopen("hello_world.txt","w");
+fwrite(message,sizeof(char),strlen(message),file);
+fclose(file);
+
+//read a message from file
+file=fopen("hello_world.txt","r");
+fread(read_message,sizeof(char),strlen(message),file);
+fclose(file);
+
+// print the message
+printf("%s\n",read_message);
+return 0;
 }
